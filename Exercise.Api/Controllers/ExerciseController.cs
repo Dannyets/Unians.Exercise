521059af1dd1757f5992761c50cb604e6317c63b
Danny Etsebban <dannyets@gmail.com>
2019-06-03 12:01:18 +0300

    Added create and configrm actions

--- Exercise.Api/Controllers/ExerciseController.cs
+++ Exercise.Api/Controllers/ExerciseController.cs
@@ -6,8 +6,6 @@ using Exercise.Api.Interfaces;
 using Exercise.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace Exercise.Api.Controllers
 {
     [Route("api/v1/[controller]/[action]")]
